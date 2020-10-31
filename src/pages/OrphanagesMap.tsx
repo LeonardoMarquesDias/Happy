525e29d1155aba1434e736c870a9088d6704f23b
Leonardo Dias <leonardo_marquesdias@hotmail.com>
2020-10-31 11:23:47 +0000

    Marker Map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [40, 58],
+  iconAnchor: [20, 58],
+  popupAnchor: [170,3]
+});
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -31,10 +40,24 @@ const OrphanagesMap: React.FC = () => {
         zoom={13.25}
         style={{ width: '100%', height: '100%' }}
       >
+
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        <Marker 
+          icon={mapIcon}
+          position={[53.4063707,-2.9941775]}
+        >
+          <Popup closeButton={false} minWidth={200} maxWidth={260} className="map-popup">
+            Royal Liverpool Seamen's Orphan Institution
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+
+        </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
